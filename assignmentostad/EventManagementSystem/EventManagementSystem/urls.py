76d19cd83d27eb701e2829589896b8f301c91967
zahid-sust <zahid_sust@yahoo.com>
2024-10-31 23:39:42 +0600

    Update urls.py

--- assignmentostad/EventManagementSystem/EventManagementSystem/urls.py
+++ assignmentostad/EventManagementSystem/EventManagementSystem/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('events.urls')),  # Include URLs from the events app
+    path('', include('events.urls')),  
 ]
